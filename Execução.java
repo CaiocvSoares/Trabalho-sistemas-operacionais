2f2fa976beeda2bb85ce751b0c0a3ffa076d666c
Caio Caldas <caiocaldasvsoares@gmail.com>
2025-10-17 18:44:16 -0300

    Ideia Base

--- Execução.java
+++ Execução.java
@@ -1,3 +1,49 @@
 public class Execução {
+    float mediatempresp, mediatempesp, mediaturn;
+    int quantum = 0;
+
+    //Modelo
     
+    void Elementos(){
+        //Fazer alguma logica pra pegar elementos para aplicar 
+    }
+
+    void calcularFIFO (){
+        /*
+        calcular tempo de espera
+        calcular tempo de execução
+        calcular tempo de resposta
+        Calcular turnaround (tempo de espera + execução)
+        soma acumulativa e media
+        */
+        
+    }
+    void calcularSJF (){
+        /*
+        calcular tempo de espera
+        calcular tempo de execução
+        calcular tempo de resposta
+        Calcular turnaround (tempo de espera + execução)
+        soma acumulativa e media
+        */
+    }
+    void calcularSRT(){
+        /*
+        calcular tempo de espera
+        calcular tempo de execução
+        calcular tempo de resposta
+        Calcular turnaround (tempo de espera + execução)
+        soma acumulativa e media
+        */
+    }
+    void calcularRR (){
+        /*
+        fazer de acordo com o quantum
+        calcular tempo de espera
+        calcular tempo de execução
+        calcular tempo de resposta
+        Calcular turnaround (tempo de espera + execução)
+        soma acumulativa e media
+        */
+    }
 }
